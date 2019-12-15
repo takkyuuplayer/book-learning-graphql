537a6a83e8aaebf53d126577be0acfc4b07de8df
takkyuuplayer <takkyuuplayer@gmail.com>
2019-12-15 10:52:08 -0500

    Many-to-Many relation

--- src/server.ts
+++ src/server.ts
@@ -16,6 +16,7 @@ type Photo {
     description: String
     category: PhotoCategory!
     postedBy: User!
+    taggedUsers: [User!]!
 }
 type User {
     githubLogin: ID!
@@ -49,6 +50,10 @@ interface IUser {
     name: String
     avatar: String
 }
+interface ITag {
+    photoID: string
+    userID: string
+}
 
 const users = [
   {'githubLogin': 'g1', 'name': 'Alice'},
@@ -59,6 +64,12 @@ const photos: Array<IPhoto> = [
   {id: ulid(), name: 'p1', description: 'd1', category: 'ACTION', githubUser: 'g1'},
   {id: ulid(), name: 'p2', description: 'd2', category: 'SELFIE', githubUser: 'g2'},
   {id: ulid(), name: 'p3', description: 'd3', category: 'LANDSCAPE', githubUser: 'g2'},
+];
+const tags: Array<ITag> = [
+  {'photoID': photos[0].id, 'userID': 'g1'},
+  {'photoID': photos[1].id, 'userID': 'g1'},
+  {'photoID': photos[1].id, 'userID': 'g2'},
+  {'photoID': photos[1].id, 'userID': 'g3'},
 ];
 
 const resolvers = {
@@ -80,6 +91,10 @@ const resolvers = {
   Photo: {
     url: (parent: IPhoto) => `https://yoursite.com/img/${parent.id}.jpg`,
     postedBy: (parent: IPhoto) => users.find((u) => u.githubLogin === parent.githubUser),
+    taggedUsers: (parent: IPhoto) => tags
+        .filter((tag) => tag.photoID === parent.id)
+        .map((tag) => tag.userID)
+        .map((userID) => users.find((u) => u.githubLogin === userID)),
   },
   User: {
     postedPhotos: (parent: IUser) => photos.filter((p) => p.githubUser === parent.githubLogin),
