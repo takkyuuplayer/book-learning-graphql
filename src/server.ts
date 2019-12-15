8eadf7596df7246ecbc13969e4b521e235471b36
takkyuuplayer <takkyuuplayer@gmail.com>
2019-12-15 08:55:47 -0500

    Add Photo

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,47 @@
+import {ulid} from 'ulid';
 import {ApolloServer} from 'apollo-server';
 const typeDefs = `
 type Query {
     totalPhotos: Int!
+    allPhotos: [Photo!]!
 }
 type Mutation {
-    postPhoto(name: String! description: String): Boolean!
+    postPhoto(name: String! description: String): Photo!
 }
-`
-;
+type Photo {
+    id: ID!
+    url: String!
+    name: String!
+    description: String
+}
+`;
 
-const photos = [];
+interface IPhoto {
+    id: string
+    url: string
+    name: string
+    description: string
+}
+
+const photos: Array<IPhoto> = [];
 
 const resolvers = {
   Query: {
     totalPhotos: () => photos.length,
+    allPhotos: () => photos,
   },
   Mutation: {
     postPhoto(parent: any, args: any) {
-      photos.push(args);
-      return true;
+      const newPhoto = {
+        id: ulid(),
+        ...args,
+      };
+      photos.push(newPhoto);
+      return newPhoto;
     },
+  },
+  Photo: {
+    url: (parent: IPhoto) => `https://yoursite.com/img/${parent.id}.jpg`,
   },
 };
 
