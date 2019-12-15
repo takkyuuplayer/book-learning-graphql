aecccf43ac30e2dd083e1eb9449660c681219691
takkyuuplayer <takkyuuplayer@gmail.com>
2019-12-15 13:00:57 -0500

    Use apollo-graphql-express

---
+++ src/resolvers/index.ts
@@ -0,0 +1,73 @@
+import {ulid} from 'ulid';
+import {GraphQLScalarType} from 'graphql';
+
+interface IPhoto {
+    id: string
+    name: string
+    description: string
+    category: string
+    githubUser: string
+    created: string
+}
+interface IUser {
+    githubLogin: string
+    name: String
+    avatar: String
+}
+interface ITag {
+    photoID: string
+    userID: string
+}
+
+const users = [
+  {'githubLogin': 'g1', 'name': 'Alice'},
+  {'githubLogin': 'g2', 'name': 'Bob'},
+  {'githubLogin': 'g3', 'name': 'Charles'},
+];
+const photos: Array<IPhoto> = [
+  {id: ulid(), name: 'p1', description: 'd1', category: 'ACTION', githubUser: 'g1', created: '3-28-1997'},
+  {id: ulid(), name: 'p2', description: 'd2', category: 'SELFIE', githubUser: 'g2', created: '1-2-1985'},
+  {id: ulid(), name: 'p3', description: 'd3', category: 'LANDSCAPE', githubUser: 'g2', created: '2018-04-15T19:09:09.308Z'},
+];
+const tags: Array<ITag> = [
+  {'photoID': photos[0].id, 'userID': 'g1'},
+  {'photoID': photos[1].id, 'userID': 'g1'},
+  {'photoID': photos[1].id, 'userID': 'g2'},
+  {'photoID': photos[1].id, 'userID': 'g3'},
+];
+
+export const Query = {
+  totalPhotos: () => photos.length,
+  allPhotos: () => photos,
+  allUsers: () => users,
+};
+export const Mutation = {
+  postPhoto(parent: any, args: any) {
+    const newPhoto = {
+      id: ulid(),
+      ...args.input,
+      created: new Date(),
+    };
+    photos.push(newPhoto);
+    return newPhoto;
+  },
+};
+export const Photo = {
+  url: (parent: IPhoto) => `https://yoursite.com/img/${parent.id}.jpg`,
+  postedBy: (parent: IPhoto) => users.find((u) => u.githubLogin === parent.githubUser),
+  taggedUsers: (parent: IPhoto) => tags
+      .filter((tag) => tag.photoID === parent.id)
+      .map((tag) => tag.userID)
+      .map((userID) => users.find((u) => u.githubLogin === userID)),
+};
+export const User = {
+  postedPhotos: (parent: IUser) => photos.filter((p) => p.githubUser === parent.githubLogin),
+};
+export const DateTime = new GraphQLScalarType({
+  name: 'DateTime',
+  description: 'A valid date time value.',
+  parseValue: (value: string) => new Date(value),
+  serialize: (value: string) => new Date(value).toISOString(),
+  parseLiteral: (ast: any) => ast.value,
+});
+
--- src/server.ts
+++ src/server.ts
@@ -1,123 +1,22 @@
-import {ulid} from 'ulid';
-import {ApolloServer} from 'apollo-server';
-import {GraphQLScalarType} from 'graphql';
-const typeDefs = `
-scalar DateTime
-type Query {
-    totalPhotos: Int!
-    allPhotos(after: DateTime): [Photo!]!
-    allUsers: [User!]!
-}
-type Mutation {
-    postPhoto(input: PostPhotoInput!): Photo!
-}
-type Photo {
-    id: ID!
-    url: String!
-    name: String!
-    description: String
-    category: PhotoCategory!
-    created: DateTime!
-    postedBy: User!
-    taggedUsers: [User!]!
-}
-type User {
-    githubLogin: ID!
-    name: String
-    avatar: String
-    postedPhotos: [Photo!]!
-}
-input PostPhotoInput {
-    name: String!
-    category: PhotoCategory = PORTRAIT
-    description: String
-}
-enum PhotoCategory {
-    SELFIE
-    PORTRAIT
-    ACTION
-    LANDSCAPE
-    GRAPHIC
-}
-`;
+import {ApolloServer} from 'apollo-server-express';
+import express from 'express';
+import expressPlayground from 'graphql-playground-middleware-express';
+import {readFileSync} from 'fs';
+import * as resolvers from './resolvers';
 
-interface IPhoto {
-    id: string
-    name: string
-    description: string
-    category: string
-    githubUser: string
-    created: string
-}
-interface IUser {
-    githubLogin: string
-    name: String
-    avatar: String
-}
-interface ITag {
-    photoID: string
-    userID: string
-}
+const typeDefs = readFileSync('./typeDef.graphql', 'UTF-8');
 
-const users = [
-  {'githubLogin': 'g1', 'name': 'Alice'},
-  {'githubLogin': 'g2', 'name': 'Bob'},
-  {'githubLogin': 'g3', 'name': 'Charles'},
-];
-const photos: Array<IPhoto> = [
-  {id: ulid(), name: 'p1', description: 'd1', category: 'ACTION', githubUser: 'g1', created: '3-28-1997'},
-  {id: ulid(), name: 'p2', description: 'd2', category: 'SELFIE', githubUser: 'g2', created: '1-2-1985'},
-  {id: ulid(), name: 'p3', description: 'd3', category: 'LANDSCAPE', githubUser: 'g2', created: '2018-04-15T19:09:09.308Z'},
-];
-const tags: Array<ITag> = [
-  {'photoID': photos[0].id, 'userID': 'g1'},
-  {'photoID': photos[1].id, 'userID': 'g1'},
-  {'photoID': photos[1].id, 'userID': 'g2'},
-  {'photoID': photos[1].id, 'userID': 'g3'},
-];
-
-const resolvers = {
-  Query: {
-    totalPhotos: () => photos.length,
-    allPhotos: () => photos,
-    allUsers: () => users,
-  },
-  Mutation: {
-    postPhoto(parent: any, args: any) {
-      const newPhoto = {
-        id: ulid(),
-        ...args.input,
-        created: new Date(),
-      };
-      photos.push(newPhoto);
-      return newPhoto;
-    },
-  },
-  Photo: {
-    url: (parent: IPhoto) => `https://yoursite.com/img/${parent.id}.jpg`,
-    postedBy: (parent: IPhoto) => users.find((u) => u.githubLogin === parent.githubUser),
-    taggedUsers: (parent: IPhoto) => tags
-        .filter((tag) => tag.photoID === parent.id)
-        .map((tag) => tag.userID)
-        .map((userID) => users.find((u) => u.githubLogin === userID)),
-  },
-  User: {
-    postedPhotos: (parent: IUser) => photos.filter((p) => p.githubUser === parent.githubLogin),
-  },
-  DateTime: new GraphQLScalarType({
-    name: 'DateTime',
-    description: 'A valid date time value.',
-    parseValue: (value: string) => new Date(value),
-    serialize: (value: string) => new Date(value).toISOString(),
-    parseLiteral: (ast: any) => ast.value,
-  }),
-};
+const app = express();
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
+server.applyMiddleware({app});
 
-server
-    .listen()
-    .then(({url}) => console.log(`GraphQL Service running on ${url}`));
+app.get('/', (req, res) => res.end('Welcome to PhotoShare API'));
+app.get('/playground', expressPlayground({endpoint: '/graphql'}));
+app
+    .listen({port: 4000}, () =>
+      console.log(`GraphQL Service running on http://localhost:4000/${server.graphqlPath}`),
+    );
