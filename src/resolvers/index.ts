6dd41b8dbc114e5cd8a471069f99d70564eca5a3
takkyuuplayer <takkyuuplayer@gmail.com>
2019-12-15 13:25:03 -0500

    Install prettier

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,39 +1,60 @@
-import {ulid} from 'ulid';
-import {GraphQLScalarType} from 'graphql';
+import { ulid } from 'ulid';
+import { GraphQLScalarType } from 'graphql';
 
 interface IPhoto {
-    id: string
-    name: string
-    description: string
-    category: string
-    githubUser: string
-    created: string
+  id: string;
+  name: string;
+  description: string;
+  category: string;
+  githubUser: string;
+  created: string;
 }
 interface IUser {
-    githubLogin: string
-    name: String
-    avatar: String
+  githubLogin: string;
+  name: String;
+  avatar: String;
 }
 interface ITag {
-    photoID: string
-    userID: string
+  photoID: string;
+  userID: string;
 }
 
 const users = [
-  {'githubLogin': 'g1', 'name': 'Alice'},
-  {'githubLogin': 'g2', 'name': 'Bob'},
-  {'githubLogin': 'g3', 'name': 'Charles'},
+  { githubLogin: 'g1', name: 'Alice' },
+  { githubLogin: 'g2', name: 'Bob' },
+  { githubLogin: 'g3', name: 'Charles' },
 ];
 const photos: Array<IPhoto> = [
-  {id: ulid(), name: 'p1', description: 'd1', category: 'ACTION', githubUser: 'g1', created: '3-28-1997'},
-  {id: ulid(), name: 'p2', description: 'd2', category: 'SELFIE', githubUser: 'g2', created: '1-2-1985'},
-  {id: ulid(), name: 'p3', description: 'd3', category: 'LANDSCAPE', githubUser: 'g2', created: '2018-04-15T19:09:09.308Z'},
+  {
+    id: ulid(),
+    name: 'p1',
+    description: 'd1',
+    category: 'ACTION',
+    githubUser: 'g1',
+    created: '3-28-1997',
+  },
+  {
+    id: ulid(),
+    name: 'p2',
+    description: 'd2',
+    category: 'SELFIE',
+    githubUser: 'g2',
+    created: '1-2-1985',
+  },
+  {
+    id: ulid(),
+    name: 'p3',
+    description: 'd3',
+    category: 'LANDSCAPE',
+    githubUser: 'g2',
+    created: '2018-04-15T19:09:09.308Z',
+  },
 ];
 const tags: Array<ITag> = [
-  {'photoID': photos[0].id, 'userID': 'g1'},
-  {'photoID': photos[1].id, 'userID': 'g1'},
-  {'photoID': photos[1].id, 'userID': 'g2'},
-  {'photoID': photos[1].id, 'userID': 'g3'},
+  { photoID: photos[0].id, userID: 'g1' },
+  { photoID: photos[1].id, userID: 'g1' },
+  { photoID: photos[1].id, userID: 'g2' },
+  { photoID: photos[1].id, userID: 'g3' },
 ];
 
 export const Query = {
@@ -54,14 +75,17 @@ export const Mutation = {
 };
 export const Photo = {
   url: (parent: IPhoto) => `https://yoursite.com/img/${parent.id}.jpg`,
-  postedBy: (parent: IPhoto) => users.find((u) => u.githubLogin === parent.githubUser),
-  taggedUsers: (parent: IPhoto) => tags
-      .filter((tag) => tag.photoID === parent.id)
-      .map((tag) => tag.userID)
-      .map((userID) => users.find((u) => u.githubLogin === userID)),
+  postedBy: (parent: IPhoto) =>
+    users.find(u => u.githubLogin === parent.githubUser),
+  taggedUsers: (parent: IPhoto) =>
+    tags
+      .filter(tag => tag.photoID === parent.id)
+      .map(tag => tag.userID)
+      .map(userID => users.find(u => u.githubLogin === userID)),
 };
 export const User = {
-  postedPhotos: (parent: IUser) => photos.filter((p) => p.githubUser === parent.githubLogin),
+  postedPhotos: (parent: IUser) =>
+    photos.filter(p => p.githubUser === parent.githubLogin),
 };
 export const DateTime = new GraphQLScalarType({
   name: 'DateTime',
@@ -70,4 +94,3 @@ export const DateTime = new GraphQLScalarType({
   serialize: (value: string) => new Date(value).toISOString(),
   parseLiteral: (ast: any) => ast.value,
 });
-
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
-import {ApolloServer} from 'apollo-server-express';
+import { ApolloServer } from 'apollo-server-express';
 import express from 'express';
 import expressPlayground from 'graphql-playground-middleware-express';
-import {readFileSync} from 'fs';
+import { readFileSync } from 'fs';
 import * as resolvers from './resolvers';
 
 const typeDefs = readFileSync('./typeDef.graphql', 'UTF-8');
@@ -12,11 +12,12 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
-server.applyMiddleware({app});
+server.applyMiddleware({ app });
 
 app.get('/', (req, res) => res.end('Welcome to PhotoShare API'));
-app.get('/playground', expressPlayground({endpoint: '/graphql'}));
-app
-    .listen({port: 4000}, () =>
-      console.log(`GraphQL Service running on http://localhost:4000/${server.graphqlPath}`),
-    );
+app.get('/playground', expressPlayground({ endpoint: '/graphql' }));
+app.listen({ port: 4000 }, () =>
+  console.log(
+    `GraphQL Service running on http://localhost:4000/${server.graphqlPath}`,
+  ),
+);
