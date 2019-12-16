e9ac1faaf863e0251673f50a4e4aa4feec41d90c
takkyuuplayer <takkyuuplayer@gmail.com>
2019-12-15 22:31:32 -0500

    Add photo

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,13 +1,13 @@
-import { ulid } from 'ulid';
 import { GraphQLScalarType } from 'graphql';
 import { authorizeWithGithub } from '../lib';
 
 interface IPhoto {
+  _id: string;
   id: string;
   name: string;
   description: string;
   category: string;
-  githubUser: string;
+  userID: string;
   created: string;
 }
 interface IUser {
@@ -15,48 +15,6 @@ interface IUser {
   name: String;
   avatar: String;
 }
-interface ITag {
-  photoID: string;
-  userID: string;
-}
-
-const users = [
-  { githubLogin: 'g1', name: 'Alice' },
-  { githubLogin: 'g2', name: 'Bob' },
-  { githubLogin: 'g3', name: 'Charles' },
-];
-const photos: Array<IPhoto> = [
-  {
-    id: ulid(),
-    name: 'p1',
-    description: 'd1',
-    category: 'ACTION',
-    githubUser: 'g1',
-    created: '3-28-1997',
-  },
-  {
-    id: ulid(),
-    name: 'p2',
-    description: 'd2',
-    category: 'SELFIE',
-    githubUser: 'g2',
-    created: '1-2-1985',
-  },
-  {
-    id: ulid(),
-    name: 'p3',
-    description: 'd3',
-    category: 'LANDSCAPE',
-    githubUser: 'g2',
-    created: '2018-04-15T19:09:09.308Z',
-  },
-];
-const tags: Array<ITag> = [
-  { photoID: photos[0].id, userID: 'g1' },
-  { photoID: photos[1].id, userID: 'g1' },
-  { photoID: photos[1].id, userID: 'g2' },
-  { photoID: photos[1].id, userID: 'g3' },
-];
 
 export const Query = {
   totalPhotos(parent: any, args: any, context: any) {
@@ -82,13 +40,20 @@ export const Query = {
   },
 };
 export const Mutation = {
-  postPhoto(parent: any, args: any) {
+  async postPhoto(parent: any, args: any, context: any) {
+    if (!context.currentUser) {
+      throw new Error('only an logged in user can post a photo');
+    }
     const newPhoto = {
-      id: ulid(),
       ...args.input,
+      userID: context.currentUser.githubLogin,
       created: new Date(),
     };
-    photos.push(newPhoto);
+    const { insertedIds } = await context.db
+      .collection('photos')
+      .insert(newPhoto);
+    newPhoto.id = insertedIds[0];
+
     return newPhoto;
   },
   async githubAuth(parent: any, args: any, context: any) {
@@ -122,18 +87,14 @@ export const Mutation = {
   },
 };
 export const Photo = {
-  url: (parent: IPhoto) => `https://yoursite.com/img/${parent.id}.jpg`,
-  postedBy: (parent: IPhoto) =>
-    users.find(u => u.githubLogin === parent.githubUser),
-  taggedUsers: (parent: IPhoto) =>
-    tags
-      .filter(tag => tag.photoID === parent.id)
-      .map(tag => tag.userID)
-      .map(userID => users.find(u => u.githubLogin === userID)),
+  id: (parent: any) => parent.id || parent._id,
+  url: (parent: IPhoto) => `https://yoursite.com/img/${parent._id}.jpg`,
+  postedBy: (parent: IPhoto, args: any, context: any) =>
+    context.db.collection('users').findOne({ githubLogin: parent.userID }),
 };
 export const User = {
-  postedPhotos: (parent: IUser) =>
-    photos.filter(p => p.githubUser === parent.githubLogin),
+  postedPhotos: (parent: IUser, args: any, context: any) =>
+    context.db.collection('photos').find({ userID: parent.githubLogin }),
 };
 export const DateTime = new GraphQLScalarType({
   name: 'DateTime',
