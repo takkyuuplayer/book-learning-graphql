0202733fa2d216c7f7ca5e5bf22acdd0b70363b6
takkyuuplayer <takkyuuplayer@gmail.com>
2019-12-24 01:17:09 -0500

    query

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,4 +1,28 @@
-import React from 'react';
+import { gql } from 'apollo-boost'
+import { ApolloClient } from 'apollo-client';
+import fetch from 'node-fetch';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from "apollo-cache-inmemory";
+
+const link = createHttpLink({ uri: 'http://localhost:4000/graphql', fetch: fetch as any });
+
+const client = new ApolloClient({
+    link: link,
+    cache: new InMemoryCache(),
+})
+
+const query = gql`
+{
+    totalUsers
+    totalPhotos
+}
+`
+
+client.query({query})
+    .then(({data}) => console.log('data', data))
+    .catch(console.error)
+
+/* import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -10,3 +34,4 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+ */
