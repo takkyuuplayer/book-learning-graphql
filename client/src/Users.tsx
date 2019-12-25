7e2289278ea8ca3e346d94e0967c117b474aac9b
takkyuuplayer <takkyuuplayer@gmail.com>
2019-12-25 10:13:32 -0500

    Add mutation

--- client/src/Users.tsx
+++ client/src/Users.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
-import { Query } from 'react-apollo';
+import { Query, Mutation } from 'react-apollo';
 import { ROOT_QUERY } from './App';
 import { User } from '../../src/generated/graphql';
 import { ApolloQueryResult } from 'apollo-boost';
+import { gql } from 'apollo-boost'
 
 interface Data {
   totalUsers: number;
   allUsers: Array<User>;
 }
 
+const ADD_FAKE_USERS_MUTATION = gql`
+  mutation addFakeUsers($count: Int!) {
+    addFakeUsers(count: $count) {
+      githubLogin
+      name
+      avatar
+    }
+  }
+`;
 const Users = () => (
   <Query<Data> query={ROOT_QUERY} pollInterval={1000 * 10}>
     {({ data, loading, refetch }) =>
@@ -37,6 +47,13 @@ const UserList = ({
   <div>
     <p>{count} Users</p>
     <button onClick={() => refetchUsers()}>Refetch Users</button>
+    <Mutation<User>
+      mutation={ADD_FAKE_USERS_MUTATION}
+      variables={{ count: 1 }}
+      refetchQueries={[{ query: ROOT_QUERY }]}
+    >
+      {addFakeUsers => <button onClick={() => addFakeUsers()}>Add Fake Users</button>}
+    </Mutation>
     <ul>
       {users.map(user => (
         <UserListItem key={user.githubLogin} {...user} />
