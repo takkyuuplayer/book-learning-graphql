82abfd12e3b06041ac5ac18f4cc85b7b29e58e50
takkyuuplayer <takkyuuplayer@gmail.com>
2019-12-25 00:24:55 -0500

    Add Query

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,15 @@
 import React from 'react';
-import Users from './Users'
-import { gql } from 'apollo-boost'
-import './App.css';
-
+import Users from './Users';
+import { gql } from 'apollo-boost';
 export const ROOT_QUERY = gql`
-query allUsers {
-  totalUsers
-  allUsers {
-    githubLogin
-    name
-    avatar
+  query allUsers {
+    totalUsers
+    allUsers {
+      githubLogin
+      name
+      avatar
+    }
   }
-}
-`
-
-const App = () => <Users />
-
-export default App
+`;
+const App = () => <Users />;
+export default App;
--- client/src/Users.tsx
+++ client/src/Users.tsx
@@ -1,10 +1,42 @@
-import React from 'react'
-import { Query } from 'react-apollo'
-import { ROOT_QUERY } from './App'
-const Users = () =>
-    {
-        return <Query query={ROOT_QUERY}>
-            {(result: any) => <p>Users are loading: {result.loading ? "yes" : "no"}</p>}
-        </Query>
+import React from 'react';
+import { Query } from 'react-apollo';
+import { ROOT_QUERY } from './App';
+import { User } from '../../src/generated/graphql';
+const Users = () => (
+  <Query query={ROOT_QUERY} pollInterval={1000 * 10}>
+    {({ data, loading, refetch }: any) =>
+      loading ? (
+        <p>loading users...</p>
+      ) : (
+        <UserList
+          count={data.totalUsers}
+          users={data.allUsers}
+          refetchUsers={refetch}
+        />
+      )
     }
-export default Users
+  </Query>
+);
+interface UserListInput {
+  count: number;
+  users: Array<User>;
+  refetchUsers: any 
+}
+const UserList = ({ count, users, refetchUsers }: UserListInput) => (
+  <div>
+    <p>{count} Users</p>
+    <button onClick={() => refetchUsers()}>Refetch Users</button>
+    <ul>
+      {users.map(user => (
+        <UserListItem key={user.githubLogin} {...user} />
+      ))}
+    </ul>
+  </div>
+);
+const UserListItem = ({ name, avatar }: User) => (
+  <li>
+    <img src={avatar as string | undefined} width={48} height={48} alt="" />
+    {name}
+  </li>
+);
+export default Users;
